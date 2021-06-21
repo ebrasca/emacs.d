2fa64fd86cbcc372c4a8ba99b0b2aa951ec8bfbe
Bruno Cichon <ebrasca@librepanther.com>
2021-06-21 18:32:20 +0000

    Separate customize interface from init.el

--- init.el
+++ init.el
@@ -1,10 +1,5 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(custom-enabled-themes '(misterioso)))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 ;; Delete trailing whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
